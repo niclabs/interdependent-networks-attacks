6f936af434366d8078fa0241e8a059778c40e2db
Ivana Bachmann <ivana.bachmann@gmail.com>
2025-03-18 15:50:45 -0300

    changed random node selection (now uses randrange and pop). Order of node deletion must be checked to obtain the correct NOI.

--- random_attack.py
+++ random_attack.py
@@ -14,21 +14,29 @@ class PhysicalRandomAttack(AbstractAttack):
         physical_nodes = physical_network.vs['name']
         number_of_physical_nodes = len(physical_nodes)
 
+        # establish sample to draw nodes from
         node_sample = physical_network.vs["name"]
 
+        # assign initial list of nodes to attack (starts empty)
         list_of_nodes_to_attack = []
+        # gl values will be stored in a list
         gl_list = []
+
+        # start loop to remove physical nodes one by one
         for i in range(1, number_of_physical_nodes):
             if use_increasing_sample:
                 if len(node_sample) > 0:
-                    node = (random.sample(node_sample, 1))[0]
+                    # add a node to the list of nodes to attack and remove
+                    # it from the sample
+                    picked_node_index = random.randrange(0,len(node_sample))
+                    node = node_sample.pop(picked_node_index)
                     list_of_nodes_to_attack.append(node)
-                    node_sample.remove(node)
                 else:
                     last_was_total_destruction = True
             else:
                 list_of_nodes_to_attack = random.sample(node_sample, i)
             GL_per_iteration = interdependent_graph.remove_physical_nodes(list_of_nodes_to_attack)
+            print(GL_per_iteration)
             gl_list.append(GL_per_iteration)
 
         #TODO: should this return??
