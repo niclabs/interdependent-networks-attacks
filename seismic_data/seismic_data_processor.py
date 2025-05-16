333cc79f4d8d8addb2622fcd0e60b7623ee5913c
Ivana Bachmann <ivana.bachmann@gmail.com>
2025-05-16 17:49:35 -0400

    added documentation. Code was refactored and cleaned.

--- seismic_data/seismic_data_processor.py
+++ seismic_data/seismic_data_processor.py
@@ -1,7 +1,5 @@
 """ This script contains all the functions associated to seismic data
 processing.
-
-TODO: documentation
 """
 
 import csv
@@ -9,62 +7,70 @@ import numpy
 import os
 
 
-def get_chile_pga(Mw, H, Feve, R, St_t, Vs30):
-    c1 = -2.8548
-    c2 = 0.7741
-    c3 = -0.97558
-    c4 = 0.1
-    c5 = -0.00174
-    c6 = 5
-    c7 = 0.35
-    c8 = 0.00586
-    c9 = -0.03958
-    delta_c1 = 2.5699
-    delta_c2 = -0.4761
-    delta_c3 = -0.52745
-    h0 = 50  # 50 km
-    Mr = 5
-    Vref = 1530  # 1530 m/s
-
-    if Feve == 0:
-        delta_fm = c9 * (Mw ** 2)
-    else:
-        delta_fm = delta_c1 + delta_c2 * Mw
-
-    Ff = c1 + c2*Mw + c8*(H - h0)*Feve + delta_fm
-
-    g = (c3 + c4*(Mw - Mr) + delta_c3*Feve)
-    R0 = ((1 - Feve)*c6 * 10**(c7*(Mw - Mr)))
-
-    Fd = g*numpy.log10(R + R0) + c5*R
-
-    Fs = St_t*numpy.log10(Vs30/Vref)
-
-    log10_pga = Ff + Fd + Fs
-
-    pga = 10 ** log10_pga
-    return pga
-
 def soil_value(vs30):
+    """ Returns the soil coefficient for a given vs30. Currently hardcoded
+
+    :param vs30: float
+        Average shear wave velocity down to 30m depth
+    :return: float
+        Soil coefficient
+    """
     soil_coef = -0.506
-    #print("Using Soil coef: {}".format(soil_coef))
     return soil_coef#-0.322
 
 
 def km_to_coordinates_chile(kms):
+    """ Converts kilometers to 'coordinate units' as used in the space in
+    which the networks are embedded.
+
+    Units were set using Chile as reference.
+
+    :param kms: float
+    :return: float
+        coordinates units
+    """
     coordinate = 20
     km = 175
     factor = coordinate / km  # "coordinate" per km
     return kms * factor
 
+
 def coordinates_to_km_chile(coordinates):
+    """ Converts 'coordinate units' to kilometers as used in the space in
+    which the networks are embedded.
+
+    :param coordinates: float
+    :return: float
+        kilometers
+    """
     coordinate = 20
     kms = 175
     factor = kms/coordinate # kms per unit of "coordinate"
     return coordinates*factor
 
-def get_chile_pga2(Mw, H, Feve, R, St_t, Vs30):
-    #print("Using eq for T = 0.3s")
+
+def get_chile_pga_T03(Mw, H, Feve, R, St_t, Vs30):
+    """ Returns the ground acceleration for the given parameters according
+    to the ground motion prediction equations presented by Idini et al.
+    for the chilean territory. Uses equation for time period T = 0.3s.
+
+    :param Mw: float
+        Moment magnitude of the seismic event
+    :param H: float
+        Hypocentral depth in meters
+    :param Feve: int
+        Represents whether the event is an interface event (Feve = 0) or
+        an intraslab event (Feve = 1)
+    :param R: float
+        Hypocentral distance in meters
+    :param St_t: float
+        Site effect coefficient given by the local soil
+    :param Vs30: float
+        Average shear wave velocity down to 30m depth
+    :return: float
+        Ground acceleration
+    """
+    # set coefficients as specified in Idini's work for T=0.3s
     c1 = -3.5422
     c2 = 0.9441
     c3 = -0.84814
@@ -80,66 +86,115 @@ def get_chile_pga2(Mw, H, Feve, R, St_t, Vs30):
     h0 = 50  # 50 km
     Mr = 5
     Vref = 1530  # 1530 m/s
-    #print("Using soil coef: {}".format(St_t))
+
+    # obtain values depending on the event type
     if Feve == 0:
         delta_fm = c9 * (Mw ** 2)
     else:
         delta_fm = delta_c1 + delta_c2 * Mw
 
-    Ff = c1 + c2*Mw + c8*(H - h0)*Feve + delta_fm
+    # obtain seismic source contribution
+    Ff = c1 + c2 * Mw + c8 * (H - h0) * Feve + delta_fm
 
-    g = (c3 + c4*(Mw - Mr) + delta_c3*Feve)
-    R0 = ((1 - Feve)*c6 * 10**(c7*(Mw - Mr)))
+    # obtain path contribution
+    g = (c3 + c4 * (Mw - Mr) + delta_c3 * Feve)
+    R0 = ((1 - Feve) * c6 * 10 ** (c7 * (Mw - Mr)))
+    Fd = g * numpy.log10(R + R0) + c5 * R
 
-    Fd = g*numpy.log10(R + R0) + c5*R
+    # obtain local site effects
+    Fs = St_t * numpy.log10((Vs30 + 0.00000000000001) / Vref)
 
-    Fs = St_t*numpy.log10((Vs30+0.00000000000001)/Vref)
-
+    # get log of the predicted ground acceleration
     log10_pga = Ff + Fd + Fs
 
+    # get ground acceleration
     pga = 10 ** log10_pga
     return pga
 
-def linear_shindo_scale_probability(pga):
-    #print("Using linear probabilities")
-    gravity_acceleration = 9.81
-    ms_pga = pga * gravity_acceleration
-    # print("-------------")
-    # print("pga: {}, ms_pga: {}".format(pga, ms_pga))
-    if ms_pga > 4:
-        return 1
-    else:
-        prob_value = two_point_line_eq(ms_pga, (0.06, 0.0), (6.0, 1.0))
-        if prob_value < 0:
-            prob_value = 0
-        # print("---> failure prob: {}".format(prob_value))
-        # print("-------------")
-        return prob_value
 
-def shindo_scale_probability(pga):
+def linear_shindo_scale_probability(pga):
+    """ Returns the failure probability of a node given the predicted
+    ground acceleration it experiences.
+
+    :param pga: float
+        predicted ground acceleration of a node
+    :return: float
+        failure probability (values between 0 and 1)
+    """
+
+    gravity_acceleration = 9.81
+    # assign a value for tha 'shindo' scale
+    max_ms_pga = 4  # TODO: Why saturates at 4? (I forgor :c )
+    linear_function = saturated_two_point_line_eq(max_ms_pga, (0.06, 0.0), (6.0, 1.0))
+    return shindo_scale_probability(pga, linear_function)
+
+def shindo_scale_probability(pga, probability_function):
+    """ Returns the failure probability of a node given the predicted
+    ground acceleration it experiences, and a probability function.
+
+    :param pga: float
+        predicted ground acceleration of a node
+    :param probability_function: function
+        must go between 0 and 1
+    :return: float
+        failure probability (values between 0 and 1)
+    """
+    gravity_acceleration = 9.81  # TODO: was pga actual acceleration? why gravity??
+    # assign a value for tha 'shindo' scale
+    ms_pga = pga * gravity_acceleration
+
+    # assign probability value using a linear equation
+    prob_value = probability_function(ms_pga)
+
+    if prob_value < 0:
+        prob_value = 0
+    elif prob_value > 1:
+        prob_value = 1
+
+    return prob_value
+
+
+def stair_shindo_scale_probability(pga):
+    """ Returns the failure probability of a node given the predicted
+    ground acceleration it experiences. Here a staircase function is
+    used. Between limits a linear function is used.
+
+    :param pga: float
+        predicted ground acceleration of a node
+    :return: float
+        failure probability (values between 0 and 1)
+    """
     gravity_acceleration = 9.8
     ms_pga = pga * gravity_acceleration
     tiers = {#"0": (0, 0.008),
              #"1": (0.008, 0.025),
              #"2": (0.025, 0.08),
              #"3": (0.08,0.25),
-             "4": [(0.25, 0.80), (0.01, 0.1)], # delta = 0.55 -> 0.8 m/s => 10% damage probability
-             "5-": [(0.8, 1.4), (0.1, 0.2)], # delta = 0.6 -> 1.4 m/s => 20% damage probability
-             "5+": [(1.4, 2.5), (0.2, 0.5)], # delta = 1.1 -> 2.5 m/s => 50% damage probability
-             "6-": [(2.5, 3.15), (0.5, 0.85)], # delta = 0.65 -> 3.15 m/s => 85% damage probability
-             "6+": [(3.15, 4), (0.85, 1)], # delta = 0.85 -> 4 m/s => 100% damage probability
-             "7": [(4, float('inf')), (1, 1)]}
+             "4": [(0.025, 0.80), (0.01, 0.1)], # delta = 0.55 -> 0.8 m/s => 10% damage probability
+             "5-": [(0.8, 1.4),(0.1,0.2)], # delta = 0.6 -> 1.4 m/s => 20% damage probability
+             "5+": [(1.4, 2.5),(0.2,0.5)], # delta = 1.1 -> 2.5 m/s => 50% damage probability
+             "6-": [(2.5, 3.15),(0.5,0.85)], # delta = 0.65 -> 3.15 m/s => 85% damage probability
+             "6+": [(3.15, 4),(0.85,1)], #delta = 0.85 -> 4 m/s => 100% damage probability
+             "7": [(4, float('inf')),(1,1)]}
 
     for tier_number in tiers:
         if tiers[tier_number][0][0] <= ms_pga < tiers[tier_number][0][1]:
-            point_1 = (tiers[tier_number][0][0], tiers[tier_number][1][0])
-            point_2 = (tiers[tier_number][0][1], tiers[tier_number][1][1])
-            prob_value = two_point_line_eq(ms_pga, point_1, point_2)
+            point_1 = (tiers[tier_number][0][0],tiers[tier_number][1][0])
+            point_2 = (tiers[tier_number][0][1],tiers[tier_number][1][1])
+            prob_value = _two_point_line_eq(ms_pga, point_1, point_2)
             return prob_value
     return 0
 
 def load_seismic_data_from_file(seismic_data_file):
-    data_dict = load_from_csv(seismic_data_file)
+    """ Load seismic events from a csv file.
+
+    :param seismic_data_file: str
+        file path
+    :return: list
+        contains dictionaries with the information associated to each
+        seismic event
+    """
+    data_dict = load_seismic_events_from_csv(seismic_data_file)
     seismic_data_list = []
     for event_id in data_dict.keys():
         event = data_dict[event_id][0]
@@ -161,7 +216,14 @@ def site_reference(site_type):
     return values[site_type]
 
 
-def load_from_csv(csv_file):
+def load_seismic_events_from_csv(csv_file):
+    """ Returns a dictionary with the data of each seismic event in
+    a csv file containing the information.
+
+    :param csv_file: str
+        file path
+    :return: dict
+    """
 
     first_row = True
     first_row_dict = {}
@@ -201,134 +263,18 @@ def load_from_csv(csv_file):
     return data_dict
 
 
-def get_chile_vs30(Mw, H, Feve, R, St_t, pga):
-    c1 = -2.8548
-    c2 = 0.7741
-    c3 = -0.97558
-    c4 = 0.1
-    c5 = -0.00174
-    c6 = 5
-    c7 = 0.35
-    c8 = 0.00586
-    c9 = -0.03958
-    delta_c1 = 2.5699
-    delta_c2 = -0.4761
-    delta_c3 = -0.52745
-    h0 = 50  # 50 km
-    Mr = 5
-    Vref = 1530  # 1530 m/s
-
-    if Feve == 0:
-        delta_fm = c9 * (Mw ** 2)
-    else:
-        delta_fm = delta_c1 + delta_c2 * Mw
-
-    Ff = c1 + c2*Mw + c8*(H - h0)*Feve + delta_fm
-
-    g = (c3 + c4*(Mw - Mr) + delta_c3*Feve)
-    R0 = ((1 - Feve)*c6 * 10**(c7*(Mw - Mr)))
-
-    Fd = g*numpy.log10(R + R0) + c5*R
-
-    log10_pga = numpy.log10(pga)
-
-    if St_t != 0:
-        Vs30 = (10 ** ((log10_pga - Ff - Fd) / St_t)) * Vref
-    else:
-        Vs30 = Vref
-
-    return Vs30
-
-
-def get_vs30_from_data_in_csv(csv_file):
-    data = load_from_csv(csv_file)
-    vs30_list = []
-    for event_id in data:
-        for event in data[event_id]:
-            Mw = event["Magnitude"]
-            H = event["Depth"]
-            Feve = event["Fault Type"]
-            R = event["Distance"]
-            site_type = event["T* Class"]
-            St_t = site_reference(site_type)
-            pga = event["PGA"]
-            vs30 = get_chile_vs30(Mw, H, Feve, R, St_t, pga)
-            vs30_list.append({"T* Class": site_type, "Vs30": vs30})
-    return vs30_list
-
-
-def shindo_scale_probability(pga):
-    gravity_acceleration = 9.8
-    ms_pga = pga * gravity_acceleration
-    tiers = {#"0": (0, 0.008),
-             #"1": (0.008, 0.025),
-             #"2": (0.025, 0.08),
-             #"3": (0.08,0.25),
-             "4": [(0.025, 0.80), (0.01, 0.1)], # delta = 0.55 -> 0.8 m/s => 10% damage probability
-             "5-": [(0.8, 1.4),(0.1,0.2)], # delta = 0.6 -> 1.4 m/s => 20% damage probability
-             "5+": [(1.4, 2.5),(0.2,0.5)], # delta = 1.1 -> 2.5 m/s => 50% damage probability
-             "6-": [(2.5, 3.15),(0.5,0.85)], # delta = 0.65 -> 3.15 m/s => 85% damage probability
-             "6+": [(3.15, 4),(0.85,1)], #delta = 0.85 -> 4 m/s => 100% damage probability
-             "7": [(4, float('inf')),(1,1)]}
-
-    for tier_number in tiers:
-        if tiers[tier_number][0][0] <= ms_pga < tiers[tier_number][0][1]:
-            point_1 = (tiers[tier_number][0][0],tiers[tier_number][1][0])
-            point_2 = (tiers[tier_number][0][1],tiers[tier_number][1][1])
-            prob_value = two_point_line_eq(ms_pga, point_1, point_2)
-            return prob_value
-    return 0
-
-
-def two_point_line_eq(x, point_1, point_2):
+def _two_point_line_eq(x, point_1, point_2):
     x1 = point_1[0]
     y1 = point_1[1]
     x2 = point_2[0]
     y2 = point_2[1]
     return (y2 - y1) * ((x - x1)/(x2 - x1)) + y1
 
-def my_init():
-    pgas = []
-    soils = ['III']
-    for soil in soils:
-        St_t = site_reference(soil)
-        Mw = 8.8
-        Feve = 0
-        R = 100.5
-        H = 28.1
-        Vs30 = 700
+def saturated_two_point_line_eq(max_val, point_1, point_2):
+    x1 = point_1[0]
+    y1 = point_1[1]
+    x2 = point_2[0]
+    y2 = point_2[1]
+    linear_saturated_function = lambda x: (y2 - y1) * ((x - x1)/(x2 - x1)) + y1 if x <= max_val else 1.0
+    return linear_saturated_function
 
-        R = 50
-        pga = get_chile_pga(Mw, H, Feve, R, St_t, Vs30)
-        pgas.append(pga)
-        shindo = shindo_scale_probability(pga)
-        print("soil {} : pga = {}, shindo prob = {} ".format(soil, pga, shindo))
-
-        R = 100
-        pga = get_chile_pga(Mw, H, Feve, R, St_t, Vs30)
-        pgas.append(pga)
-        shindo = shindo_scale_probability(pga)
-        print("soil {} : pga = {}, shindo prob = {} ".format(soil, pga, shindo))
-
-        R = 200
-        pga = get_chile_pga(Mw, H, Feve, R, St_t, Vs30)
-        pgas.append(pga)
-        shindo = shindo_scale_probability(pga)
-        print("soil {} : pga = {}, shindo prob = {} ".format(soil, pga, shindo))
-
-
-    dip = load_from_csv('seismic_data.csv')
-
-    for k in dip.keys():
-        print(k)
-        print(dip[k])
-
-def test():
-
-    a = load_from_csv('seismic_data.csv')
-
-
-    ssum = 0
-
-    for k in a.keys():
-        ssum += len(a[k])
